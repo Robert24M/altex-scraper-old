66dabda8eb42eabaf0950539e7134a90fd32a6c0
Robert <micu.robert24@gmail.com>
2024-06-03 19:13:41 +0300

    feat: load first page from smartphones products

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.zip.GZIPInputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("https://fenrir.altex.ro/catalog/category/telefoane?size=48"))
+                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+                .header("Accept-Encoding", "gzip, deflate, br, zstd")
+                .header("Accept-Language", "en-US,en;q=0.9")
+                .header("Cache-Control", "max-age=0")
+                .header("Priority", "u=0, i")
+                .header("Sec-Ch-Ua", "Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24")
+                .header("Sec-Ch-Ua-Mobile", "?0")
+                .header("Sec-Ch-Ua-Platform", "Windows")
+                .header("Sec-Fetch-Dest", "document")
+                .header("Sec-Fetch-Mode", "navigate")
+                .header("Sec-Fetch-Site", "none")
+                .header("Sec-Fetch-User", "?1")
+                .header("Upgrade-Insecure-Requests", "1")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+                .GET()
+                .build();
+
+        HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+        String encoding = response.headers().firstValue("Content-Encoding").orElse("");
+
+        String responseBody;
+        if ("gzip".equalsIgnoreCase(encoding)) {
+            try (GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(response.body()));
+                 InputStreamReader reader = new InputStreamReader(gis);
+                 BufferedReader in = new BufferedReader(reader)) {
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                }
+                responseBody = sb.toString();
+            }
+        } else {
+            responseBody = new String(response.body());
+        }
+
+        System.out.println(responseBody);
     }
 }
